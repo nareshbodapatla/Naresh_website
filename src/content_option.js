8e3bf85cbb8fa483808cd62693ff78df71ca2da0
Naresh Bodapatla <nareshbodapat@CLPT2041.local>
2024-09-01 18:00:22 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,149 @@
+const logotext = "Naresh";
+const meta = {
+    title: "Naresh Bodapatla",
+    description: "I’m Naresh Bodapatla _ Product Support Engineer,currently working in Capillary",
+};
+
+const introdata = {
+    title: "I’m Naresh",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web applications",
+    },
+    description: "Passionate software developer with expertise in MERN STACK Development. I build scalable, innovative solutions, focusing on clean code and agile practices. Driven by continuous learning, I thrive in collaborative, problem-solving environments.",
+    your_img_url: "https://img.freepik.com/free-photo/3d-rendering-kid-playing-digital-game_23-2150898496.jpg",
+};
+
+const dataabout = {
+    title: "Abit about my self",
+    aboutme: "Passionate software developer with expertise in MERN STACK Development. I build scalable, innovative solutions, focusing on clean code and agile practices. Driven by continuous learning, I thrive in collaborative, problem-solving environments.",
+};
+const worktimeline = [{
+        img:"https://pbs.twimg.com/profile_images/1333623004761370624/MGfXVFGm_400x400.jpg",
+        jobtitle: "Product Support Engineer",
+        where: "Capillary Bangalore",
+        date: "2024 - Present",
+    },
+    {
+        jobtitle: "MERN STACK Developer Intern",
+        img:"https://d8it4huxumps7.cloudfront.net/uploads/images/150x150/uploadedManual-65e5e573eb9af_whatsapp_image_2024-01-17_at_10.27.18_am-removebg-preview.png",
+        where: "Opam Technologies, Pune(Remote)",
+        date: "2024",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 80,
+    },
+    {
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 80,
+    },
+    {
+        name: "Bootstrap",
+        value: 90,
+    },
+    {
+        name: "MongoDB",
+        value: 70,
+    },
+    {
+        name: "SQL",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Node",
+        value: 75,
+    },
+];
+
+{/*
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Web Applications",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+*/}
+
+const dataportfolio = [{
+        img: "https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018",
+        description: "Hot & Ready is an eatery that offers fast and fresh dishes that are perfect for dining in or enjoying in the comfort of your home.",
+        link: "https://foodpandabnk.ccbp.tech/",
+    },
+    {
+        img: "https://static.vecteezy.com/system/resources/previews/023/783/293/non_2x/artificial-intelligence-generated-icon-ai-sign-for-graphic-design-logo-website-social-media-mobile-app-ui-illustration-vector.jpg",
+        description: "Generative AI is a type of artificial intelligence technology that can produce various types of content, including text, imagery, audio and synthetic data.",
+        link: "https://nareshgenaichai.ccbp.tech/",
+    },
+    {
+        img: "https://static.vecteezy.com/system/resources/previews/022/123/561/original/weather-app-icon-outline-style-isolated-on-white-background-free-vector.jpg",
+        description: "This weather app is one of best free weather apps with full features: Local weather, weather map (weather map service) and weather widgets.",
+        link: "https://github.com/nareshbodapatla/simpleweatherapp",
+    },
+    {
+        img: "https://t4.ftcdn.net/jpg/05/13/72/29/360_F_513722905_SgxiGdjQZsdvP4ODmERsQGgW2bUwj1lT.jpg",
+        description: "A job search app that combines professional networking and job searching. You can search for jobs, apply, set up job alerts, and build a professional profile.",
+        link: "https://github.com/nareshbodapatla/Jobby-app",
+    },
+    {
+        img: "https://i.pinimg.com/originals/f8/98/bf/f898bfb34a80f0784e1417c86a096e13.png",
+        description: "Task manager is a utility program that is included with the Windows operating system (OS). Its primary purpose is to allow users to monitor and manage the processes and applications running on their computer.",
+        link: "https://github.com/nareshbodapatla/Task-manager-mern",
+    },
+    {
+        img: "https://img.freepik.com/free-vector/twitter-new-2023-x-logo-white-background-vector_1017-45422.jpg",
+        description: "Twitter is a free social networking site where users broadcast short posts known as tweets",
+        link: "https://github.com/nareshbodapatla/Twetter_API",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "itsmenaressh@gmail.com",
+    YOUR_FONE: "9542464568",
+    description: "Passionate software developer with expertise in MERN STACK Development. I build scalable, innovative solutions, focusing on clean code and agile practices. Driven by continuous learning, I thrive in collaborative, problem-solving environments.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/nareshbodapatla",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com/in/bodapatlanaresh",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
